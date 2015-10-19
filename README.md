5a67984bff605d058a9dc6757a078d3c59b4d20f
Dmytro Pishchukhin <demon@Demons-MBP.fritz.box>
2015-10-19 19:24:49 +0200

    refactoring

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # keepass-maven-plugin
 Maven plugin to integrate KeePass into build process
+
+[![Build Status](https://travis-ci.org/knowhowlab/keepass-maven-plugin.svg?branch=master)](https://travis-ci.org/knowhowlab/keepass-maven-plugin)
--- src/main/java/org/knowhowlab/maven/plugins/keepass/ReadMojo.java
+++ src/main/java/org/knowhowlab/maven/plugins/keepass/ReadMojo.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2010-2015 Dmytro Pishchukhin (http://knowhowlab.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.knowhowlab.maven.plugins.keepass;
 
 import de.slackspace.openkeepass.KeePassDatabase;
@@ -12,14 +28,16 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.String.format;
 import static org.apache.maven.plugins.annotations.LifecyclePhase.VALIDATE;
 
 /**
  * @author dpishchukhin.
  */
-@Mojo(name = "read", defaultPhase = VALIDATE)
+@Mojo(name = "read", defaultPhase = VALIDATE, threadSafe = true)
 public class ReadMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project}", readonly = true)
     private MavenProject project;
@@ -40,24 +58,51 @@ public class ReadMojo extends AbstractMojo {
      * Properties
      */
     @Parameter(required = true)
-    private Record record;
+    private List<Record> records = new ArrayList<Record>();
 
     public void execute() throws MojoExecutionException, MojoFailureException {
-        KeePassFile keePassFile = KeePassDatabase.getInstance(file).openDatabase(password);
+        KeePassFile keePassFile;
+        try {
+            keePassFile = KeePassDatabase.getInstance(file).openDatabase(password);
+        } catch (Exception e) {
+            getLog().error(format("Unable to open file: %s", file.getAbsolutePath()));
+            throw new MojoFailureException(format("Unable to open file: %s", file.getAbsolutePath()));
+        }
 
+        for (Record record : records) {
+            handleRecord(keePassFile, record);
+        }
+    }
+
+    private void handleRecord(KeePassFile keePassFile, Record record) throws MojoFailureException {
         Group group = keePassFile.getRoot();
         if (record.getGroup() != null) {
             group = findGroup(group, record.getGroup());
-        }
-
-        List<Entry> entries = group.getEntries();
-        for (Entry entry : entries) {
-            if (entry.getTitle().equals(record.getTitle())) {
-                project.getProperties().setProperty(record.getPrefix() + "username", entry.getUsername());
-                project.getProperties().setProperty(record.getPrefix() + "password", entry.getPassword());
-                project.getProperties().setProperty(record.getPrefix() + "url", entry.getUrl());
+            if (group == null) {
+                getLog().error(format("Group name: %s is unknown", record.getGroup()));
+                throw new MojoFailureException(format("Group name: %s is unknown", record.getGroup()));
             }
         }
+
+        Entry entry = findEntry(group, record.getTitle());
+        if (entry == null) {
+            getLog().error(format("Entry title: %s is unknown", record.getTitle()));
+            throw new MojoFailureException(format("Entry title: %s is unknown", record.getTitle()));
+        }
+
+        project.getProperties().setProperty(record.getPrefix() + "username", entry.getUsername());
+        project.getProperties().setProperty(record.getPrefix() + "password", entry.getPassword());
+        project.getProperties().setProperty(record.getPrefix() + "url", entry.getUrl());
+    }
+
+    private Entry findEntry(Group group, String title) {
+        List<Entry> entries = group.getEntries();
+        for (Entry entry : entries) {
+            if (entry.getTitle().equals(title)) {
+                return entry;
+            }
+        }
+        return null;
     }
 
     private Group findGroup(Group parent, String groupName) {
